b85e10749aeba1a0d214b2fec0367e96210553db
Eyad Wahdan <Eyadwahdan02@gmail.com>
2022-01-06 22:36:15 +0100

    fix route in Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <img alt="PlanIt logo" src="../assets/logo.png" height= "35" width= "35">
-      <a class="navbar-brand" href="#">PlanIt</a>
+      <a class="navbar-brand" href="/">PlanIt</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
