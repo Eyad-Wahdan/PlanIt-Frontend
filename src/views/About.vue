4989e177acc2a342fd5ce782ef8fea2ba9185e20
Wadim Bader <Wadim.Bader@Student.htw-berlin.de>
2022-01-06 20:23:35 +0100

    add styling to aboutpage

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <style>
 .wrapper {
-  max-width: 90%;
+  max-width: 100%;
   padding: 100px 20px 100px 100px;
   text-align: left;
 }
@@ -27,25 +27,33 @@
 }
 
 .box {
-  margin-top: 20px;
   display: flex;
-  flex-direction: row;
   width: 100%;
 }
 
 .picture {
-  height: auto;
+  padding-right: 60px;
+}
+
+.work{
+  max-width: 45%;
+  padding-top: 70px;
+}
+.body{
+  width: 100%;
+  color: #fff;
+  background: linear-gradient(#2493ef, #2c4ac5, #150386);
 }
 </style>
 
 <template>
-  <body>
-  <div class="picture">
-    <img src="../assets/planen.jpg" height="650" width="850" alt="Bild" style="float:right">
-  </div>
+  <body class="body">
   <section class="wrapper">
     <div>
       <h1> About PlanIt</h1>
+      <div class="picture">
+        <img src="../assets/Planen.png" height="389" width="704" alt="Bild" style="float:right">
+      </div>
       <h4 class="heading_2"> Leichter gemacht das Leben zu managen</h4>
       <p class="paragraph">Ein neues Semester hat begonnen und schon jetzt müssen der Großteil der Studierenden sich
         vielen Herausforderungen stellen.
@@ -53,6 +61,11 @@
         Dabei die Übersicht zu verlieren, kann schon einmal schnell passieren.
         <br>
         Wir haben die Lösung für euch: PlanIt!</p>
+      <div class="work">
+      <h3>Wie funktioniert PlanIt?</h3>
+      <p>Sobald man PlanIt startet gelangt man auf die Homepage. Von da aus kann man auf "Lets get started" klicken und man gelangt damit auf "Events".
+        Da kann man auf einen "Termin erstellen" klicken und dann das Datum und die Uhrzeit angeben, wann der Termin stattfinden soll.</p>
+    </div>
     </div>
   </section>
   <div class="box">
