d79d65bd41bb9bdf31c1b7a9ec3c1fc86e493ffe
Wadim Bader <Wadim.Bader@Student.htw-berlin.de>
2022-01-07 23:07:02 +0100

    modify Styling in Home- and Aboutpage

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,43 @@
+<template>
+  <body class="body">
+  <section class="wrapper">
+    <div>
+      <h1> About PlanIt</h1>
+      <div class="picture">
+        <img src="../assets/Planen.png" height="389" width="704" alt="Bild" style="float:right">
+      </div>
+      <h4 class="heading_2"> Leichter gemacht das Leben zu managen</h4>
+      <p class="paragraph">Ein neues Semester hat begonnen und schon jetzt müssen der Großteil der Studierenden sich
+        vielen Herausforderungen stellen.
+        Von großen Projektarbeiten hin zu kleineren Abgaben und den schon bald bevorstehenden Semesterklausuren.
+        Dabei die Übersicht zu verlieren, kann schon einmal schnell passieren.
+        <br>
+        Wir haben die Lösung für euch: PlanIt!</p>
+      <div class="work">
+        <h3>Wie funktioniert PlanIt?</h3>
+        <p>Sobald man PlanIt startet gelangt man auf die Homepage. Von da aus kann man auf "Lets get started" klicken und man gelangt damit auf "Events".
+          Da füllt man dann die freien Felder "Date", "Start-Time", "Finish-Time" und "Event" aus und das war´s dann auch schon. Sehr einfach und sehr schnell.</p>
+      </div>
+    </div>
+  </section>
+  <div class="box">
+    <div class="warum_planit">
+      <h3>Warum PlanIt?</h3>
+      <p>PlanIt ist ein sehr benutzerfreundliches Programm, das sehr übersichtlich und einfach anwendbar ist. Man kann in nur wenigen Sekunden einen Event erstellen und wieder Löschen.</p>
+    </div>
+    <div class="für_wen">
+      <h3>Für wen ist PlanIt?</h3>
+      <p>PlanIt ist für jeden gedacht. Egal ob Schüler, Student oder sogar Unternehmensleiter. Jeder kann seinen Alltag
+        mit PlanIt leichter planen und organisieren.</p>
+    </div>
+  </div>
+  </body>
+</template>
+
 <style>
 .wrapper {
   max-width: 100%;
-  padding: 100px 20px 100px 100px;
+  padding: 81px 20px 70px 100px;
   text-align: left;
 }
 
@@ -40,44 +76,9 @@
   padding-top: 70px;
 }
 .body{
+  height: 100vh;
   width: 100%;
   color: #fff;
-  background: linear-gradient(#2493ef, #2c4ac5, #150386);
+  background: linear-gradient(#090042, #011c93, #090042);
 }
 </style>
-
-<template>
-  <body class="body">
-  <section class="wrapper">
-    <div>
-      <h1> About PlanIt</h1>
-      <div class="picture">
-        <img src="../assets/Planen.png" height="389" width="704" alt="Bild" style="float:right">
-      </div>
-      <h4 class="heading_2"> Leichter gemacht das Leben zu managen</h4>
-      <p class="paragraph">Ein neues Semester hat begonnen und schon jetzt müssen der Großteil der Studierenden sich
-        vielen Herausforderungen stellen.
-        Von großen Projektarbeiten hin zu kleineren Abgaben und den schon bald bevorstehenden Semesterklausuren.
-        Dabei die Übersicht zu verlieren, kann schon einmal schnell passieren.
-        <br>
-        Wir haben die Lösung für euch: PlanIt!</p>
-      <div class="work">
-        <h3>Wie funktioniert PlanIt?</h3>
-        <p>Sobald man PlanIt startet gelangt man auf die Homepage. Von da aus kann man auf "Lets get started" klicken und man gelangt damit auf "Events".
-          Da kann man auf einen "Termin erstellen" klicken und dann das Datum und die Uhrzeit angeben, wann der Termin stattfinden soll.</p>
-      </div>
-    </div>
-  </section>
-  <div class="box">
-    <div class="warum_planit">
-      <h3>Warum PlanIt?</h3>
-      <p>PlanIt ist ein sehr benutzerfreundliches Programm, das sehr übersichtlich und einfach anwendbar ist.</p>
-    </div>
-    <div class="für_wen">
-      <h3>Für wen ist PlanIt?</h3>
-      <p>PlanIt ist für jeden gedacht. Egal ob Schüler, Student oder sogar Unternehmensleiter. Jeder kann seinen Alltag
-        mit PlanIt leichter planen und organisieren.</p>
-    </div>
-  </div>
-  </body>
-</template>
